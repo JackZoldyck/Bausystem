617f6a858432eec615a85f92fdb5719c16010730
Jack Zoldyck <fun.frock2004@gmail.com>
2026-06-21 14:45:49 +0200

    Add scene and assets

--- Assets/Scripts/ResourceNode.cs
+++ Assets/Scripts/ResourceNode.cs
@@ -6,18 +6,26 @@ public class ResourceNode : MonoBehaviour
     public int woodAmount = 10;
     public int health = 3;
     public float respawnTime = 60f;
+    public GameObject stumpObject;
 
     private int maxHealth;
     private Renderer[] renderers;
     private Collider[] colliders;
     private TreeHitFeedback feedback;
+    private MeshRenderer meshRenderer;
+    private Collider treeCollider;
 
     void Start()
     {
         maxHealth = health;
-        renderers = GetComponentsInChildren<Renderer>();
-        colliders = GetComponentsInChildren<Collider>();
+        renderers = GetComponentsInChildren<Renderer>(true);
+        colliders = GetComponentsInChildren<Collider>(true);
         feedback = GetComponent<TreeHitFeedback>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        treeCollider = GetComponent<Collider>();
+
+        if (stumpObject != null)
+            stumpObject.SetActive(false);
     }
 
     public void Harvest(PlayerInventory inventory, int damage, Vector3 hitPoint, Vector3 hitNormal)
@@ -29,6 +37,14 @@ public class ResourceNode : MonoBehaviour
         if (health <= 0)
         {
             inventory.wood += woodAmount;
+
+            WoodGainPopup popup =
+                FindFirstObjectByType<WoodGainPopup>();
+
+            if (popup != null)
+            {
+                popup.ShowWoodGain(woodAmount);
+            }
 
             InventoryUI inventoryUI = FindFirstObjectByType<InventoryUI>();
 
@@ -43,18 +59,35 @@ public class ResourceNode : MonoBehaviour
     {
         SetTreeActive(false);
 
+        if (stumpObject != null)
+            stumpObject.SetActive(true);
+
         yield return new WaitForSeconds(respawnTime);
 
         health = maxHealth;
+
         SetTreeActive(true);
+
+        if (stumpObject != null)
+            stumpObject.SetActive(false);
     }
 
     void SetTreeActive(bool active)
     {
         foreach (Renderer renderer in renderers)
+        {
+            if (stumpObject != null && renderer.transform.IsChildOf(stumpObject.transform))
+                continue;
+
             renderer.enabled = active;
+        }
 
         foreach (Collider collider in colliders)
+        {
+            if (stumpObject != null && collider.transform.IsChildOf(stumpObject.transform))
+                continue;
+
             collider.enabled = active;
+        }
     }
 }
